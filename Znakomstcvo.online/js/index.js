d46232ef7646015b631fca4ec9e4ff2d2de64407
z1t3x <91964299+z1t3x@users.noreply.github.com>
2023-02-18 19:13:15 +0200

    Add files via upload

---
+++ Znakomstcvo.online/js/index.js
@@ -0,0 +1,57 @@
+//let string = "qwerty";
+//let integer = 2;
+//let float = 5.66789098765;
+//let bool = true;
+//let bool1 = false;
+//let newstring = string + " hello world";
+//
+////console.log(integer + " " + float);
+//
+////alert(string)
+//function vivod(qwe) {
+//    console.log("Я функцианирую");
+//    alert(qwe * qwe);
+//}
+//
+////vivod(4)
+////vivod(7)
+////vivod(2)
+////prompt(5)
+//
+////let age = prompt("Ваш возраст");
+////console.log(age)
+//
+////if (age<18){
+////    alert("Вход воспрещен");
+////}
+////else if (age>115) {
+////    alert("Вы слишоком стары(")
+////}
+////else {
+////    alert("Добро пожаловать!");
+////}
+//
+////let fromnum = parseInt(prompt("От какого числа"),10)
+////let tonum = parseInt(prompt("До какого"),10)
+////let usernumber = parseInt(prompt("Попытайте удачу"), 10)
+////let number = randomInteger(fromnum, tonum)
+////
+////if (usernumber < fromnum || usernumber > tonum) {
+////    alert("Введено неверное число")
+////} else if (usernumber === number) {
+////    alert("Вы выиграли")
+////} else {
+////    alert("Вы проиграли, правильное число:")
+////    alert(number)
+////}
+////
+////function randomInteger(min, max) {
+////    let rand = min + Math.random() * (max + 1 - min);
+////    return Math.floor(rand);
+////}
+//
+//
+//let num1 = 2
+//let num2 = 12
+//
+//console.log(Math.pow(num1, 70))
